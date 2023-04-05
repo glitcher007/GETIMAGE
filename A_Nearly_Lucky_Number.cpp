a393b7cf48ae8c4f2492a9335a983b20aa703b88
glitcher007 <112776999+glitcher007@users.noreply.github.com>
2023-04-06 02:17:07 +0530

    Add files via upload

---
+++ A_Nearly_Lucky_Number.cpp
@@ -0,0 +1,644 @@
+/*⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡎⠀⠀⢀⣠⣴⣾⣿⣿⣿⡄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡶⠃⠀⣀⣠⣴⣾⣿⣿⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⣿⣟⣥⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣔⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⠿⠟⠛⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⠟⠋⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⢀⠠⢀⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⢿⣿⡿⠋⢀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣾⣷⣿⣿⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡟⠁⣿⠟⠀⡀⠀⢀⣴⣾⣿⣿⣿⣦⠀⡇⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠏⠀⢸⠏⣠⠎⢀⣴⣿⡿⠟⡻⠁⠀⠀⠘⠁⠀⣤⣴⣾⡿⠛⣉⡙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠏⠀⠀⠋⠸⠁⣰⣿⣿⠉⠀⡠⠁⠀⢀⣀⡀⠀⠀⠘⢿⠋⢠⣊⡈⠙⡈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢄⣴⣠⣾⣏⣉⣡⣴⡿⠟⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⣆⢈⡀⠀⢱⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣤⣤⣤⣟⠬⠋⣵⡤⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣈⠢⠹⡄⢀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣏⣯⣟⣁⡀⠀⠀⢠⠚⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇⠀⣀⠌⠀⣫⣼⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠉⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠻⣿⣿⠏⣀⠄⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠚⠛⠀⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣥⣤⣄⣀⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠈⡽⠊⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⠤⠀⠒⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠛⠛⠛⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠗⢤⣶⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡃⠀⠀⠀⠀⠈⠙⢿⣿⣿⣿⣿⣿⡿⠷⠶⠆⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡀⠀⠀⠀⠈⢀⡼⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⠃⠀⠀⠀⠀⠀⠀⠈⢏⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢆⠀⠀⠀⠏⠀⠀⠀⠀⠀⠀⠀⣀⣠⣴⣶⣿⣿⠇⠀⣜⠀⠀⠀⠀⠀⠀⠈⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⣄⠀⠀⠀⠀⠀⣀⣤⣾⣿⣿⣿⣿⣿⣿⡟⠀⣴⡧⠀⠀⠀⠀⠀⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⢄⠀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣁⣤⣿⣶⣶⣶⣶⣶⣶⣶⣷⣶⣾⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⢿⡟⡛⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣀⣀⣀⣀⣀⣀⣀⠀⠀⣀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⢃⣧⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠯⠭⠭⠭⠭⡭⣍⣉⣉⡛⢻⣯
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣕⣒⣲⣖⠲⠶⠶⠤⠤⣤⣔⣭⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣻⣯⣭⣭⠤⠤⠤⣤⣄⣀⡉⠉⠲⡄⣪⣴⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢋⠔⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡄⠀⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⢖⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣟⣛⠯⠥⠶⠃⠤⠶⠤⠤⢤⣤⣀⠀⠀⠀⠀⠀⢠⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⠤⡻⣫⣿⣿⣿⣿⣿⣿⣿⢿⣛⠿⠛⠋⠁⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠂⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣶⣾⣟⡭⢒⣋⣿⣿⣿⣿⢟⣻⢍⠕⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡈⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⡫⢓⠥⡄⢼⣿⠿⠋⡡⢊⠜⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠃⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⠡⠊⠀⡀⠓⠁⠀⠀⠜⡐⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⡿⠃⡠⠒⠁⠀⠀⠀⠀⡜⡜⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡟⠠⠊⠀⠀⠀⠀⠀⠀⠸⢠⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⡎⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⠤⠂⢀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⢡⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣠⣤⣤⣤⣶⣶⡾⠟⣋⠅⠐⠈⠈⢆⡀⢿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⢸⡀⠀⠀⠀⠀⠀⢀⣀⣀⣤⣤⣦⣶⣶⣖⣿⣽⠿⠿⠿⠿⠽⠿⠿⢟⠫⠑⠀⠁⠀⠀⠀⠀⠀⠈⢧⠈⢿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡆⢣⢀⣠⣶⣶⠿⠟⢛⣋⡩⠭⠍⠓⠒⠂⠀⠀⠀⠀⠀⠐⡆⠐⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⣄⣹⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣖⠿⠀⣉⠭⠑⠂⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠃⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⢻⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⢀⣴⡾⢗⡩⠔⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠠⠤⠤⠀⠒⠒⠒⠒⠒⠒⢺⢠⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⢃⣾⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣄⣀⣀⣀⣤⡾⢟⠡⠊⠁⠀⠀⠀⠀⡀⠠⠔⠒⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡣⠚⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⠟⡫⠐⠁⠀⠀⢀⠀⠔⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠀⠀⠀⠀⢀⡇⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⡫⠁⠀⠊⢿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⡌⡁⡇⠀⢀⠠⠂⠁⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠒⠂⠉⠉⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⣕⠁⠀⠀⠀⡨⢿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣧⢠⢱⠊⠁⠀⠀⠀⠀⠀⡀⠄⠐⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡆⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢈⠌⠀⠑⠠⣀⠔⠁⠈⢿⣿⣿
+*/    
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define rep(i,a,K)     for(int i=a;i<K;i++)
+  
+#define ll             long long
+//#define int             long long int 
+#define  take(v,n)          for (int i = 0; i < n; ++i){int x;cin>>x; v.pb(x);}
+#define pb             push_back
+#define all(v)         v.begin(),v.end()
+#define rall(v)         v.rbegin(),v.rend()
+#define vi             vector<int>
+
+#define mii             map<int,int>
+#define fr             first
+#define sc             second
+#define gcd(a,b)       __gcd(a,b)
+#define mem1(a)        memset(a,-1,sizeof(a))
+#define mem0(a)        memset(a,0,sizeof(a))
+
+#define pii            pair<int,int>
+#define maxy           1000000007
+#define elasped_time   1.0 * clock() / CLOCKS_PER_SEC
+template<typename T>
+using minpq = priority_queue<T, vector<T>, greater<T>>;
+#define pq             priority_queue
+
+/*** BINARY INDEXED TREE ***/
+ 
+/*GET SUM OF RANGE FROM 1...to..N OF THAT ELEMENT */
+ll getSum(ll BITree[], ll index)
+{
+    ll sum = 0; // Initialize result
+  
+    // index in BITree[] is 1 more than the index in arr[]
+    index = index + 1;
+  
+    // Traverse ancestors of BITree[index]
+    while (index>0)
+    {
+        // Add current element of BITree to sum
+        sum += BITree[index];
+  
+        // Move index to parent node in getSum View
+        index -= index & (-index);
+    }
+    return sum;
+}
+ 
+/* UPDATE IN BINARY INDEXED TREE */
+void updateBIT(ll BITree[], ll n, ll index, ll val)
+{
+    // index in BITree[] is 1 more than the index in arr[]
+    index = index + 1;
+  
+    // Traverse all ancestors and add 'val'
+    while (index <= n)
+    {
+    // Add 'val' to current node of BI Tree
+    BITree[index] += val;
+  
+    // Update index to that of parent in update View
+    index += index & (-index);
+    }
+}
+/* CONSTRUCT BINARY INDEXED TREE */
+ll *constructBITree(ll arr[], ll n)
+{
+    // Create and initialize BITree[] as 0
+    ll *BITree = new ll[n+1];
+    for (ll i=1; i<=n; i++)
+        BITree[i] = 0;
+  
+    // Store the actual values in BITree[] using update()
+    for (ll i=0; i<n; i++)
+        updateBIT(BITree, n, i, arr[i]);
+  
+    // Uncomment below lines to see contents of BITree[]
+    //for (int i=1; i<=n; i++)
+    //     cout << BITree[i] << " ";
+  
+    return BITree;
+}
+ 
+/**********************/
+ 
+/****FIRST DIGIT FUNCTION*******/
+    int firstdigit(int n){
+ 
+            int digits = (int)log10(n);
+ 
+            n = (int)(n/pow(10,digits));
+        
+ 
+        return n;
+    }
+ 
+/*****NUMBER OF DIGITS FUNCTION*****/
+    int countDigit(long long n)
+     {
+        if (n==0)
+        {
+            return 1;
+        }
+        
+        else{
+            return floor(log10(n) + 1);
+        }
+    }
+ 
+/*******PRIME NUMBER FUNCTION**********/
+ll isPrime(ll n){
+    int flag=0;
+    for (int i = 2; i*i<=n; i++)
+    {
+        if (n%i==0)
+        {
+            flag=1;
+        }
+        
+    }
+    if (flag==0)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+ 
+ 
+/****************MILER-RABIN PRIMALITY TEST********************/
+ll power(ll x,unsigned ll y,ll p)
+{
+    ll res = 1;      // Initialize result
+    x = x % p;  // Update x if it is more than or
+                // equal to p
+    while (y > 0)
+    {
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res*x) % p;
+ 
+        // y must be even now
+        y = y>>1; // y = y/2
+        x = (x*x) % p;
+    }
+    return res;
+}
+ 
+// This function is called for all k trials. It returns
+// false if n is composite and returns true if n is
+// probably prime.
+// d is an odd number such that  d*2<sup>r</sup> = n-1
+// for some r >= 1
+bool miillerTest(ll d, ll n)
+{
+    // Pick a random number in [2..n-2]
+    // Corner cases make sure that n > 4
+    ll a = 2 + rand() % (n - 4);
+ 
+    // Compute a^d % n
+    ll x = power(a, d, n);
+ 
+    if (x == 1  || x == n-1)
+       return true;
+ 
+    // Keep squaring x while one of the following doesn't
+    // happen
+    // (i)   d does not reach n-1
+    // (ii)  (x^2) % n is not 1
+    // (iii) (x^2) % n is not n-1
+    while (d != n-1)
+    {
+        x = (x * x) % n;
+        d *= 2;
+ 
+        if (x == 1)      return false;
+        if (x == n-1)    return true;
+    }
+ 
+    // Return composite
+    return false;
+}
+ 
+// It returns false if n is composite and returns true if n
+// is probably prime.  k is an input parameter that determines
+// accuracy level. Higher value of k indicates more accuracy.
+bool isPrimeMillerRabin(ll n, ll k)
+{
+    // Corner cases
+    if (n <= 1 || n == 4)  return false;
+    if (n <= 3) return true;
+ 
+    // Find r such that n = 2^d * r + 1 for some r >= 1
+    ll d = n - 1;
+    while (d % 2 == 0)
+        d /= 2;
+ 
+    // Iterate given number of 'k' times
+    for (int i = 0; i < k; i++)
+         if (!miillerTest(d, n))
+              return false;
+ 
+    return true;
+}
+ 
+/**********SUM OF DIGITS FUNCTION************/
+int sumofdigits(int n){
+    int sum=0,l=countDigit(n);
+    for (int j = 1; j <=l; j++)
+    {
+        sum=sum+(n%10);
+        n=n/10;
+    }
+    return sum;
+}
+ 
+/*******FACTORIAL FUNCTION************/
+int factorial(int n){
+    if (n==0)
+    {
+       return 1;
+    }
+    return n*factorial(n-1);
+}
+ 
+/***BINARY SEARCH IN ARRAY***/
+ll binarySearch(ll arr[], ll l, ll r, ll x)
+{
+    while (l <= r) {
+        ll m = l + (r - l) / 2;
+ 
+         if (arr[m] == x)
+            return m;
+ 
+        if (arr[m] < x)
+            l = m + 1;
+         else
+            r = m - 1;
+    }
+    return -1;
+}
+ 
+/********BINARY SEARCH IN VECTOR*********/
+ll binarySearchInVector(vector<ll> v, ll To_Find)
+{
+    int lo = 0, hi = v.size() - 1;
+    int mid;
+    // This below check covers all cases , so need to check
+    // for mid=lo-(hi-lo)/2
+    while (hi - lo > 1) {
+        int mid = (hi + lo) / 2;
+        if (v[mid] < To_Find) {
+            lo = mid + 1;
+        }
+        else {
+            hi = mid;
+        }
+    }
+    if (v[lo] == To_Find) {
+        // cout << "Found"
+        //      << " At Index " << lo << endl;
+        return lo;
+    }
+    else if (v[hi] == To_Find) {
+        // cout << "Found"
+        //      << " At Index " << hi << endl;
+        return hi;
+    }
+    else {
+        return -1;
+    }
+}
+ 
+/************* UPPER BOUND IN AN ARRAY ******************/
+ll lower_bound(ll arr[], ll N, ll X)
+{
+    int mid;
+ 
+    // Initialise starting index and
+    // ending index
+    int low = 0;
+    int high = N;
+ 
+    // Till low is less than high
+    while (low < high) {
+        mid = low + (high - low) / 2;
+ 
+        // If X is less than or equal
+        // to arr[mid], then find in
+        // left subarray
+        if (X <= arr[mid]) {
+            high = mid;
+        }
+ 
+        // If X is greater arr[mid]
+        // then find in right subarray
+        else {
+            low = mid + 1;
+        }
+    }
+   
+    // if X is greater than arr[n-1]
+    if(low < N && arr[low] < X) {
+       low++;
+    }
+       
+    // Return the lower_bound index
+    return low;
+}
+ 
+/************* LOWER BOUND IN AN ARRAY ******************/
+ll upper_bound(ll arr[], ll N, ll X)
+{
+    int mid;
+ 
+    // Initialise starting index and
+    // ending index
+    int low = 0;
+    int high = N;
+ 
+    // Till low is less than high
+    while (low < high) {
+        // Find the middle index
+        mid = low + (high - low) / 2;
+ 
+        // If X is greater than or equal
+        // to arr[mid] then find
+        // in right subarray
+        if (X >= arr[mid]) {
+            low = mid + 1;
+        }
+ 
+        // If X is less than arr[mid]
+        // then find in left subarray
+        else {
+            high = mid;
+        }
+    }
+   
+    // if X is greater than arr[n-1]
+    if(low < N && arr[low] <= X) {
+       low++;
+    }
+ 
+    // Return the upper_bound index
+    return low;
+}
+ 
+ 
+/*****IS POWER OF 2******/
+bool isPowerofTwo(long long n)
+{
+    return (n != 0) && ((n & (n - 1)) == 0);
+}
+ 
+/*****highest power of two less than equal two*********/
+long long highestPowerof2(long long N)
+{
+    // if N is a power of two simply return it
+    if (!(N & (N - 1)))
+        return N;
+    // else set only the most significant bit
+    return 0x8000000000000000 >> (__builtin_clzll(N));
+}
+ 
+/******if kth bit is set or not set**********/
+int isKthBitSet(ll n, ll k)
+{
+    if (n & (1 << (k - 1)))
+    {
+        return 1;
+    }
+        // cout << "SET";
+    else{
+        return 0;
+    }
+        // cout << "NOT SET";
+}
+ 
+/******GCD******/
+ll gcd(ll a, ll b)
+{
+    return b == 0 ? a : gcd(b, a % b);   
+}
+ 
+/*********LCM**********/
+ll lcm(ll a, ll b)
+{
+    ll k=a*b;
+    k/=gcd(a,b);
+    return k;
+}
+ 
+/******ISPALINDROME*******/
+ll isPalindrome(vector<char> str)
+{
+    // Start from leftmost and rightmost corners of str
+    ll l = 0;
+    ll h = str.size() - 1;
+ 
+    // Keep comparing characters while they are same
+    while (h > l)
+    {
+        if (str[l++] != str[h--])
+        {
+            // printf("%s is not a palindrome\n", str);
+            return 0;
+        }
+    }
+    return 1;
+}
+ 
+ll isPalindrome(char str[])
+{
+    // Start from leftmost and rightmost corners of str
+    int l = 0;
+    int h = strlen(str) - 1;
+ 
+    // Keep comparing characters while they are same
+    while (h > l)
+    {
+        if (str[l++] != str[h--])
+        {
+            // printf("%s is not a palindrome\n", str);
+            return 0;
+        }
+    }
+    // printf("%s is a palindrome\n", str);
+    return 1;
+}
+ 
+/*******IsPerfectSquare*******/
+bool isPerfectSquare(long double x)
+{
+    // Find floating point value of
+    // square root of x.
+    if (x >= 0) {
+ 
+        long long sr = sqrt(x);
+         
+        // if product of square root
+        //is equal, then
+        // return T/F
+        return (sr * sr == x);
+    }
+    // else return false if n<0
+    return false;
+}
+ 
+/*BINARY EXPONENTIATION*/
+ll power(ll x, ll y, ll p)
+{
+ 
+    // Initialize answer
+    ll res = 1;
+ 
+    // Check till the number becomes zero
+    while (y > 0) {
+ 
+        // If y is odd, multiply x with result
+        if (y % 2 == 1)
+            res = (res * x);
+ 
+        // y = y/2
+        y = y >> 1;
+ 
+        // Change x to x^2
+        x = (x * x);
+    }
+    return res % p;
+}
+ 
+ 
+ 
+#define startTime clock_t start, end; start = clock();
+// #define endTime end = clock(); double time_taken = double(end - start) / double(CLOCKS_PER_SEC); cout << "Time taken by program is : " << fixed  << time_taken << setprecision(5);cout << " sec " << endl;
+#define endTime end = clock(); double time_taken = double(end - start) / double(CLOCKS_PER_SEC); cout << "Time taken by program is : " << fixed  << time_taken;cout << " sec " << endl;
+ 
+ 
+//PROBLEMS USING DP/RECURSIONS
+ 
+  
+ 
+
+const int N = 2e5+5;
+int mod=1e9+7;
+
+
+
+ll msb(ll x){
+ll res=-1;
+while(x){
+    x=x>>1;
+    res++;
+}
+return res;
+ 
+ 
+}
+
+ll ans(ll x,ll y){
+ ll res=0;
+ while(x && y){
+    ll msbx=msb(x);
+    ll msby=msb(y);
+    if(msbx!=msby)
+        break;
+    ll temp=(1ll<<msbx);
+    res+=temp;
+    x-=temp;
+    y-=temp;
+ 
+ 
+ }
+return res;
+ 
+ 
+ 
+}
+int elements(int ma[]){
+    int res = 0;
+    for(int i=0; i<26; i++){
+        if(ma[i] > 0){
+            res++;
+        }
+    }
+    return res;
+}
+
+
+
+
+const int M =2e5+3;
+ll ceil_div(ll a,ll b)
+{
+      return a%b==0 ? a/b : a/b +1;
+}
+
+void mycode(){
+  
+  string s;
+  cin>>s;
+
+ int k= s.size()
+     int count=0;
+   for (int  i = 0; i < k; i++)
+   {
+    if(s[i]=='4' || s[i]=='7') count++;
+   }
+
+   int count1=0,count2=0;
+   while(count>0){
+    if(count%10 ==4 || count%10==7)
+   { count1++;}
+    count/=10;count2++;
+   }
+
+   if(count1==count2)cout<<"YES"<<endl;
+   else cout<<"NO"<<endl;
+   
+
+ 
+
+    }
+
+
+
+
+
+ 
+signed main() {
+	  ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout.tie(0);
+
+
+   
+int t;
+cin>>t;
+    //cout<<"Case #"<<++l<<": ";
+  while(t--){
+    mycode();
+   
+}
+  
+  //mycode();
+
+  return 0;
+}
